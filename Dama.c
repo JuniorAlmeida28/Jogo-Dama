6b9602868c3aedd7b97e3db934eeb6baa53b6dbe
Junior Almeida <jrjunioralmeida@outlook.com>
2022-11-22 18:23:15 -0300

    att

--- Dama.c
+++ Dama.c
@@ -10,8 +10,8 @@ int main()
     int iColuna;    // Posição inicial.
     int iLinha;     // Posição inicial.
     int aux = 0;    // Auxiliar de posição.
-    int fColuna;    // fColuna posição para onde a peça será movida.
     int fLinha;     // fLinha posição para onde a peça será movida.
+    int fColuna;    // fColuna posição para onde a peça será movida.
     int c;
     int p;
     int auxI;
@@ -94,66 +94,104 @@ int main()
     printf("  |________________________|\n");
     printf("    0  1  2  3  4  5  6  7");
 
+    while (pretas != 0 && brancas != 0)
+    {
 
-
-    while(pretas != 0 && brancas != 0){
-
-        while (1){
-            
-            printf("\n\nJogador 1 \n");
-		    scanf("%d %d", &iColuna, &iLinha);
-		    scanf("%d %d", &fColuna, &fLinha);
-
-
-        // Movimento da peça
-        if( ((((((fLinha == iLinha+1) || (fLinha == iLinha-1)) && (( fLinha >= 0 ) && (fLinha < 10))) &&  ((fColuna == iColuna+1) && (( fColuna >= 0 ) && (fColuna < 10)))) && tab[fLinha][fColuna]==46) && tab[iLinha][iColuna] == 49) )
-
-		 {
-		    tab[iLinha][iColuna] = 46;
-            tab[fLinha][fColuna] = 49;
-            break;
-        }
-        else
+        while (1)
         {
-          printf("movimento invalido");
-          continue;
-        }
+
+            printf("\n\nJogador 1 \n");
+            scanf("%d %d", &iLinha, &iColuna);
+            scanf("%d %d", &fLinha, &fColuna);
+
+            // Captura para direita
+            if (((((fColuna == iColuna + 2) && ((fColuna >= 0) && (fColuna < 10))) && ((fLinha == iLinha + 2) && ((fLinha >= 0) && (fLinha < 10)))) && tab[iColuna][iLinha] == 49) && tab[fColuna][fLinha] == 46)
+            {
+                if ((tab[iColuna + 1][iLinha + 1] == 50) || (tab[iColuna + 1][iLinha + 1] == 52))
+                {
+                    tab[iColuna + 1][iLinha + 1] = 46;
+                }
+                else
+            {
+                printf("movimento invalido");
+                continue;
+            }
+
+                tab[iColuna][iLinha] = 46;
+                tab[fColuna][fLinha] = 49;
+
+                break;
+            }
+            // Captura para esqueda
+            if (((((fColuna == iColuna - 2) && ((fColuna >= 0) && (fColuna < 10))) && ((fLinha == iLinha + 2) && ((fLinha >= 0) && (fLinha < 10)))) && tab[iColuna][iLinha] == 1) && tab[fColuna][fLinha] == 0)
+            {
+
+                if ((tab[iColuna - 1][iLinha + 1] == 2) || (tab[iColuna - 1][iLinha + 1] == 4))
+                {
+                    tab[iColuna - 1][iLinha + 1] = 0;
+                }
+                else
+            {
+                printf("movimento invalido");
+                continue;
+            }
+
+                tab[iColuna][iLinha] = 0;
+                tab[fColuna][fLinha] = 1;
+
+                break;
+                
+            }else
+            
+
+
+            // Movimento da peça
+
+            if (((((((fColuna == iColuna + 1) || (fColuna == iColuna - 1)) && ((fColuna >= 0) && (fColuna < 10))) && (((fLinha == iLinha + 1)) && ((fLinha >= 0) && (fLinha < 10)))) && tab[fColuna][fLinha] == 46) && tab[iColuna][iLinha] == 49))
+            {
+                tab[iColuna][iLinha] = 46;
+                tab[fColuna][fLinha] = 49;
+                break;
+            }
+            else
+            {
+                printf("movimento invalido");
+                continue;
+            }
         }
         system("CLS");
 
-        //VERIFICA SE VIROU DAMA:
-        for(iLinha=0;iLinha<10;iLinha++)
-        {
-		 	if(tab[iLinha][9]==49)
-		 	{
-				tab[iLinha][9]=51;
-			}
-		 }
 
-        // IMPRESSãO DO TABULEIRO:
-    printf("    0  1  2  3  4  5  6  7");
-    printf("\n  _________________________\n");
-    for (iLinha = 0; iLinha < 8; iLinha++)
-    {
-        printf("%d |", iLinha);
+        // VERIFICA SE VIROU DAMA:
         for (iColuna = 0; iColuna < 8; iColuna++)
         {
-            printf(" %c ", tab[iColuna][iLinha]);
-            if (iColuna == 7)
+            if (tab[iColuna][7] == 49)
             {
-                printf("|%d \n", iLinha);
+                tab[iColuna][7] = 51;
             }
         }
-        printf("  |                        |\n");
-    }
-    printf("  |________________________|\n");
-    printf("    0  1  2  3  4  5  6  7");
-        }
-        
 
+        // IMPRESSãO DO TABULEIRO:
+        printf("    0  1  2  3  4  5  6  7");
+        printf("\n  _________________________\n");
+        for (iLinha = 0; iLinha < 8; iLinha++)
+        {
+            printf("%d |", iLinha);
+            for (iColuna = 0; iColuna < 8; iColuna++)
+            {
+                printf(" %c ", tab[iColuna][iLinha]);
+                if (iColuna == 7)
+                {
+                    printf("|%d \n", iLinha);
+                }
+            }
+            printf("  |                        |\n");
+        }
+        printf("  |________________________|\n");
+        printf("    0  1  2  3  4  5  6  7");
 
     
-
+    }
 
     getch();
     return 0;
